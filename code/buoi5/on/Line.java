1a6d331a693440e67593bdee985545340eaa4a03
hieus-004 <hieuchuquan22@gmail.com>
2023-08-27 17:18:57 +0700

    syncb5

---
+++ code/buoi5/on/Line.java
@@ -0,0 +1,18 @@
+package buoi5.on;
+
+public class Line {
+    private Point begin;
+    private Point end;
+
+    public Line (Point begin, Point end){
+        this.begin = begin;
+        this.end = end;
+    }
+
+    public Line (int x1, int x2, int y1, int y2){
+        this.begin = new Point(x1, y1);
+        this.end = new Point(x2, y2);
+    }
+
+    public void 
+}
